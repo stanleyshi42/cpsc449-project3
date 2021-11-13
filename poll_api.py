b5bc4d441b82ddcee4ba25a2cc829faa4f087f98
student <stanleyshi42@gmail.com>
2021-11-12 16:40:18 -0800

    .

--- poll_api.py
+++ poll_api.py
@@ -7,46 +7,6 @@ poll_id = 0
 dynamodb = boto3.resource("dynamodb", endpoint_url="http://localhost:8000")
 
 
-def create_poll_table():
-    # dynamodb = boto3.resource("dynamodb", endpoint_url="http://localhost:8000")
-    if dynamodb.Table("Polls"):
-        dynamodb.Table("Polls").delete()
-
-    table = dynamodb.create_table(
-        TableName="Polls",
-        KeySchema=[
-            {"AttributeName": "p_key", "KeyType": "HASH"},  # Partition key
-            {"AttributeName": "s_key", "KeyType": "RANGE"},  # Sort key
-        ],
-        AttributeDefinitions=[
-            {"AttributeName": "p_key", "AttributeType": "S"},
-            {"AttributeName": "s_key", "AttributeType": "S"},
-        ],
-        ProvisionedThroughput={"ReadCapacityUnits": 10, "WriteCapacityUnits": 10},
-    )
-    return table
-
-
-def put_poll(p_key, s_key, attributes):
-    # dynamodb = boto3.resource("dynamodb", endpoint_url="http://localhost:8000")
-    table = dynamodb.Table("Polls")
-    print("Adding to table:", attributes)
-    try:
-        response = table.put_item(
-            Item={
-                "p_key": p_key,
-                "s_key": s_key,
-                "attributes": attributes,
-            }
-        )
-    except Exception as e:
-        # response.status = hug.falcon.HTTP_409
-        print("ERROR: ", str(e))
-        return {"error": str(e)}
-
-    return response
-
-
 @hug.get("/health/")
 def health():
     return {"health": "alive"}
@@ -66,7 +26,6 @@ def create_poll(
 ):
     """POST a new poll; minimum 2 poll options"""
     global poll_id
-    # dynamodb = boto3.resource("dynamodb", endpoint_url="http://localhost:8000")
     table = dynamodb.Table("Polls")
 
     attributes = {}
@@ -82,14 +41,14 @@ def create_poll(
             Item={
                 "p_key": "POL#" + str(poll_id),
                 "s_key": "POL#" + str(poll_id),
-                "attributes": {"Question:": question},
+                "attributes": {"Question:": question}
             }
         )
-        poll = table.put_item(
+        table.put_item(
             Item={
                 "p_key": "POL#" + str(poll_id),
                 "s_key": "OPT#" + str(poll_id),
-                "attributes": attributes,
+                "attributes": attributes
             }
         )
         poll_id += 1
@@ -114,6 +73,7 @@ def retrieve_poll(poll_id: hug.types.number, response):
 
     poll = {}
 
+    #TODO
     try:
         response = table.get_item(Key={"p_key": poll_id, "s_key": poll_id})
         # response = table.get_item(Key={"p_key": poll_id, "s_key": opt_id})
@@ -121,35 +81,3 @@ def retrieve_poll(poll_id: hug.types.number, response):
         return {"error": str(e.response["Error"]["Message"])}
     print(response)
     return response["Item"]
-
-
-if __name__ == "__main__":
-    table = create_poll_table()
-    print("Table status:", table.table_status)
-
-    put_poll("POL#1", "POL#1", {"Question": "What's your favorite day?"})
-    attributes = {}
-    attributes["Option1"] = "Friday"
-    attributes["Option2"] = "Saturday"
-    attributes["Option3"] = "Sunday"
-    attributes["Option4"] = "Monday"
-
-    put_poll(
-        "POL#1",
-        "OPT#1",
-        attributes,
-    )
-
-    """
-    # Query
-    dynamodb = boto3.resource("dynamodb", endpoint_url="http://localhost:8000")
-    table = dynamodb.Table("Polls")
-    
-    query = table.query(KeyConditionExpression=Key("p_key").eq("POL#1"))
-    query = query["Items"]
-    
-    for item in query:
-        print(item["p_key"], " :", item["attributes"])
-    print(query)
-    print(query[0]["attributes"])
-    """
